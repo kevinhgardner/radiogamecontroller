de3bf9efe67be4e235896fe69a67596f9332ee18
Kevin Gardner <kevinhgardner@gmail.com>
2023-01-09 22:22:57 +0100

    Update

---
+++ shim.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * Blocks for driving the Kitronik :GAME Controller
+     */
+    //% color=#00A654 weight=100
+declare namespace RadioGameController {
+}
+
+// Auto-generated. Do not edit. Really.
